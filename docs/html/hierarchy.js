ddb5e3b25b93b3b599bf530b6252d013d2e9ce8d
oshkines <oshkines@mail.ru>
2026-04-11 19:05:23 +0300

    Lesson7: SFINAE + Doxygen v3.0

---
+++ docs/html/hierarchy.js
@@ -0,0 +1,17 @@
+var hierarchy =
+[
+    [ "std::false_type", null, [
+      [ "is_container< T, std::void_t< decltype(std::declval< T >().begin()), decltype(std::declval< T >().end()), typename T::value_type > >", "structis__container_3_01_t_00_01std_1_1void__t_3_01decltype_07std_1_1declval_3_01_t_01_4_07_08_8b8de4275c57050e20d163d06e3eb847c.html", null ],
+      [ "is_tuple< std::tuple< Ts... > >", "structis__tuple_3_01std_1_1tuple_3_01_ts_8_8_8_01_4_01_4.html", null ],
+      [ "tuple_types_are_equal< std::tuple< T > >", "structtuple__types__are__equal_3_01std_1_1tuple_3_01_t_01_4_01_4.html", null ],
+      [ "tuple_types_are_equal< std::tuple< T, Ts... > >", "structtuple__types__are__equal_3_01std_1_1tuple_3_01_t_00_01_ts_8_8_8_01_4_01_4.html", null ],
+      [ "is_container< T, typename >", "structis__container.html", null ],
+      [ "is_tuple< T >", "structis__tuple.html", null ],
+      [ "tuple_types_are_equal< Tuple >", "structtuple__types__are__equal.html", null ]
+    ] ],
+    [ "std::true_type", null, [
+      [ "is_container< T, std::void_t< decltype(std::declval< T >().begin()), decltype(std::declval< T >().end()), typename T::value_type > >", "structis__container_3_01_t_00_01std_1_1void__t_3_01decltype_07std_1_1declval_3_01_t_01_4_07_08_8b8de4275c57050e20d163d06e3eb847c.html", null ],
+      [ "is_tuple< std::tuple< Ts... > >", "structis__tuple_3_01std_1_1tuple_3_01_ts_8_8_8_01_4_01_4.html", null ],
+      [ "tuple_types_are_equal< std::tuple< T > >", "structtuple__types__are__equal_3_01std_1_1tuple_3_01_t_01_4_01_4.html", null ]
+    ] ]
+];
---
+++ docs/html/search/all_5.js
@@ -0,0 +1,14 @@
+var searchData=
+[
+  ['info_5farch_0',['info_arch',['../_c_make_c_compiler_id_8c.html#a59647e99d304ed33b15cb284c27ed391',1,'info_arch:&#160;CMakeCCompilerId.c'],['../_c_make_c_x_x_compiler_id_8cpp.html#a59647e99d304ed33b15cb284c27ed391',1,'info_arch:&#160;CMakeCXXCompilerId.cpp']]],
+  ['info_5fcompiler_1',['info_compiler',['../_c_make_c_compiler_id_8c.html#a4b0efeb7a5d59313986b3a0390f050f6',1,'info_compiler:&#160;CMakeCCompilerId.c'],['../_c_make_c_x_x_compiler_id_8cpp.html#a4b0efeb7a5d59313986b3a0390f050f6',1,'info_compiler:&#160;CMakeCXXCompilerId.cpp']]],
+  ['info_5flanguage_5fextensions_5fdefault_2',['info_language_extensions_default',['../_c_make_c_compiler_id_8c.html#a0f46a8a39e09d9b803c4766904fd7e99',1,'info_language_extensions_default:&#160;CMakeCCompilerId.c'],['../_c_make_c_x_x_compiler_id_8cpp.html#a0f46a8a39e09d9b803c4766904fd7e99',1,'info_language_extensions_default:&#160;CMakeCXXCompilerId.cpp']]],
+  ['info_5flanguage_5fstandard_5fdefault_3',['info_language_standard_default',['../_c_make_c_compiler_id_8c.html#a4607cccf070750927b458473ca82c090',1,'info_language_standard_default:&#160;CMakeCCompilerId.c'],['../_c_make_c_x_x_compiler_id_8cpp.html#a4607cccf070750927b458473ca82c090',1,'info_language_standard_default:&#160;CMakeCXXCompilerId.cpp']]],
+  ['info_5fplatform_4',['info_platform',['../_c_make_c_compiler_id_8c.html#a2321403dee54ee23f0c2fa849c60f7d4',1,'info_platform:&#160;CMakeCCompilerId.c'],['../_c_make_c_x_x_compiler_id_8cpp.html#a2321403dee54ee23f0c2fa849c60f7d4',1,'info_platform:&#160;CMakeCXXCompilerId.cpp']]],
+  ['is_5fcontainer_5',['is_container',['../structis__container.html',1,'']]],
+  ['is_5fcontainer_3c_20t_2c_20std_3a_3avoid_5ft_3c_20decltype_28std_3a_3adeclval_3c_20t_20_3e_28_29_2ebegin_28_29_29_2c_20decltype_28std_3a_3adeclval_3c_20t_20_3e_28_29_2eend_28_29_29_2c_20typename_20t_3a_3avalue_5ftype_20_3e_20_3e_6',['is_container&lt; T, std::void_t&lt; decltype(std::declval&lt; T &gt;().begin()), decltype(std::declval&lt; T &gt;().end()), typename T::value_type &gt; &gt;',['../structis__container_3_01_t_00_01std_1_1void__t_3_01decltype_07std_1_1declval_3_01_t_01_4_07_08_8b8de4275c57050e20d163d06e3eb847c.html',1,'']]],
+  ['is_5finteger_5ffor_5fip_7',['is_integer_for_ip',['../main_8cpp.html#ae520d88e10b44b5b78665c8a94f34440',1,'main.cpp']]],
+  ['is_5fstring_8',['is_string',['../main_8cpp.html#afc10b4c0b54027986dbd9e7e188517a1',1,'main.cpp']]],
+  ['is_5ftuple_9',['is_tuple',['../structis__tuple.html',1,'']]],
+  ['is_5ftuple_3c_20std_3a_3atuple_3c_20ts_2e_2e_2e_20_3e_20_3e_10',['is_tuple&lt; std::tuple&lt; Ts... &gt; &gt;',['../structis__tuple_3_01std_1_1tuple_3_01_ts_8_8_8_01_4_01_4.html',1,'']]]
+];
